89ac3109871602fadc0f7e7c5647259fed391177
Dicson <fetex98@gmail.com>
2020-06-21 23:00:45 -0500

    use components

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,19 +3,26 @@ import { CommonModule } from '@angular/common';
 import { HousesComponent } from './houses/houses.component';
 import { IonicModule } from '@ionic/angular';
 import { HouseComponent } from './house/house.component';
+import { MembersComponent } from './members/members.component';
+import { HouseDetailsComponent } from './house-details/house-details.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 
 @NgModule({
   declarations: [
     HousesComponent,
-    HouseComponent
+    HouseComponent,
+    HouseDetailsComponent,
+    MembersComponent
   ],
   exports: [
     HousesComponent,
+    MembersComponent
   ],
   imports: [
     CommonModule,
-    IonicModule
+    IonicModule,
+    FontAwesomeModule
   ]
 })
 export class ComponentsModule { }
